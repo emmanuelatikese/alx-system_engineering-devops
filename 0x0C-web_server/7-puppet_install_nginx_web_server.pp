05cfda6f40ae1ad6131a8e1a0e61d25079f8441b
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-22 10:24:21 -0800

    adding

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,32 +1,38 @@
 # Update package repositories
-
-exec { 'dist update':
-        command  => '/usr/bin/apt-get update',
-        provider => 'shell'
+exec { 'dist_update':
+  command  => 'sudo apt update',
+  path     => ['/bin', '/usr/bin'],  # Specify the PATH to resolve the issue
+  provider => 'shell',
 }
 
+# Install Nginx package
 package { 'nginx':
   ensure  => 'installed',
-  require => Exec['dist update']
+  require => Exec['dist_update'],
 }
 
-file {'/var/www/html/index.html':
-  path    => '/var/www/html/index.html',
+# Create index.html file
+file { '/var/www/html/index.html':
+  ensure  => file,
   content => 'Hello World!',
   owner   => $USER,
   group   => $USER,
   mode    => '0644',
 }
 
-exec {'redirect_me':
-  command  => "sed -i '20i\\
-        location ~ redirect_me {\\
-            rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\\
-        }' /etc/nginx/sites-available/default",
-  provider => 'shell'
+# Use sed to insert the location block
+exec { 'insert_redirect_location':
+  command => "sudo sed -i '20i\\
+    location ~ redirect_me {\\
+        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\\
+    }' /etc/nginx/sites-available/default",
+  require => File['/etc/nginx/sites-available/default'],
+  notify  => Service['nginx'],
 }
 
-service {'nginx':
-  ensure  => running,
-  require => Package['nginx']
+# Ensure Nginx service is running
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
 }
